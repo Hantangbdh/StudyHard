bdc503c21b981c4da345a0ad11e30d1fa76bf8be
cxy <18203992442@163.com>
2018-12-18 11:52:00 +0800

    update

--- src/main/java/Program8.java
+++ src/main/java/Program8.java
@@ -1,14 +1,21 @@
 import static java.lang.Math.pow;
 
 //求s=a+aa+aaa+aaaa+a.....a 的值  a为一个数
-/*public class Program8 {
-    for(int i=1;i<5;i++){
-        int n=2*pow(10,i-1);
+//以2为例 4项和
+public class Program8 {
+    public static void main(String[] args) {
         int sum=0;
-        sum += i;
-        System.out.print("每一项的值为：");
-        int sum_total = sum;
+        int sum_total=0;
+    for(int i=1;i<5;i++) {
+        double n = 2 * pow(10, i - 1);
+
+        sum += n;
+        System.out.print(sum+"  ");
+
         sum_total += sum;
+
+    }
+        System.out.println(sum_total);
+
     }
 }
-*/
